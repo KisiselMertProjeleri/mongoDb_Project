5adef53c566ac2034437ed12f8633d081c387c3b
BruceNymann <bruceleonymann@gmail.com>
2024-12-14 15:46:24 +0000

    booksdb ve konu anlatimi yapildi

---
+++ books_db.py
@@ -0,0 +1,15 @@
+from mongodb_connect import MongoDBConnection
+
+class BooksDB(MongoDBConnection):
+    def __init__(self, db_name):
+        super().__init__()
+        self.collection = self.database_getir(db_name)['Books']
+
+    def kitaplar_ekleme(self,data:dict):
+        self.collection.insert_many(data)
+
+    def kitap_id_ve_isim_sorgulama(self):
+        return self.collection.aggregate([
+            {'$project':{'title':1, 'isbn':1}} # pandasdaki count() methodu ile ayni is yapar
+        ])
+    
---
+++ mongodb_connect.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+class MongoDBConnection:
+    def __init__(self,url='mongodb+srv://bruceleonymann:wZa4LVqaFAhbgzIz@deneme1.19txh.mongodb.net/'):
+        self.client = MongoClient(url)
+
+    
+    def database_getir(self, db_name):
+        return self.client[db_name]
